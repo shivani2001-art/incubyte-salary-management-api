fd32fa494c30b2e5fe3135c3d26f37275866929e
shivani2001-art <shivshakannan821@gmail.com>
2026-03-20 00:04:00 +0530

    feat: implement signup endpoint (green)

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,15 @@
 class AuthenticationController < ApplicationController
-  skip_before_action :authenticate_request!, only: [:login], raise: false
+  skip_before_action :authenticate_request!, only: [:login, :signup], raise: false
+
+  def signup
+    user = User.new(email: params[:email], password: params[:password])
+    if user.save
+      token = JwtService.encode(user_id: user.id)
+      render json: { token: token }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
   def login
     user = User.find_by(email: params[:email])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
+  post "auth/signup", to: "authentication#signup"
   post "auth/login", to: "authentication#login"
 
   namespace :api do
