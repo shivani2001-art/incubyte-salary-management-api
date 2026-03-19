09ff67eb61cc3835f07bba3307843f3c0fa44e88
shivani2001-art <shivshakannan821@gmail.com>
2026-03-19 22:14:00 +0530

    feat: implement employee CRUD endpoints (green)

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,8 +1,49 @@
 module Api
   module V1
     class EmployeesController < ApplicationController
+      before_action :set_employee, only: %i[show update destroy]
+
       def index
-        render json: [], status: :ok
+        employees = Employee.all
+        render json: employees, status: :ok
+      end
+
+      def show
+        render json: @employee, status: :ok
+      end
+
+      def create
+        employee = Employee.new(employee_params)
+        if employee.save
+          render json: employee, status: :created
+        else
+          render json: { errors: employee.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @employee.update(employee_params)
+          render json: @employee, status: :ok
+        else
+          render json: { errors: @employee.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @employee.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_employee
+        @employee = Employee.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "Employee not found" }, status: :not_found
+      end
+
+      def employee_params
+        params.require(:employee).permit(:full_name, :job_title, :country, :salary)
       end
     end
   end
